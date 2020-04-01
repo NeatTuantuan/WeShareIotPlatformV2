bbceaaf0f515c4a0e7a1a00bb59756b6bd5e61c3
Mirrthon <516344765@qq.com>
2020-04-01 19:25:30 +0800

    mqtt协议服务器端暂时到connect部分 下一步检测连接以及测试发送消息

---
+++ src/main/java/com/bdi/mqtt/server/MqttServer.java
@@ -0,0 +1,61 @@
+package com.bdi.mqtt.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.*;
+import io.netty.handler.codec.mqtt.MqttDecoder;
+import io.netty.handler.codec.mqtt.MqttEncoder;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @description mqtt协议服务端(代理端)
+ * @date 20200401
+ * @author
+ */
+public class MqttServer {
+
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception{
+
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new IdleStateHandler(10, 10, 120, TimeUnit.SECONDS));//心跳机制
+                            p.addLast(MqttEncoder.INSTANCE);//编码
+                            p.addLast(new MqttDecoder());//解码
+//                            p.addLast(new MqttServerHandler(65535));
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)//等待队列
+                    .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                    .childOption(ChannelOption.ALLOCATOR,PooledByteBufAllocator.DEFAULT)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ChannelFuture f = b.bind(1883);
+            b.bind("0.0.0.0", 1883).sync().channel();
+            f.channel().closeFuture().sync();
+
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
+
